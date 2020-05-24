bda2adda1da84533032010a23ff7e61f59537fa3
jiaming5139 <821269398@qq.com>
2020-05-24 08:50:58 +0800

    create kqueue  class

---
+++ kqueue.cpp
@@ -0,0 +1,34 @@
+//
+// Created by parallels on 5/24/20.
+//
+#include "kqueue.h"
+
+#ifdef __APPLE__
+#include "kqueue.h"
+
+
+#endif
+
+kqueue::kqueue(EventLoop *loop) : Poller(loop) {
+
+}
+
+void kqueue::poller_wait(int timeOut, Poller::Channelptrlist *activedChannels) {
+
+}
+
+void kqueue::updateChannel(Poller::Channelptr) {
+
+}
+
+void kqueue::removeChannel(Poller::Channelptr) {
+
+}
+
+bool kqueue::hasChannel(Poller::Channelptr) {
+    return false;
+}
+
+kqueue::~kqueue() {
+
+}
---
+++ kqueue.h
@@ -0,0 +1,23 @@
+//
+// Created by parallels on 5/24/20.
+//
+#ifdef __linux__
+#ifndef MUDUO_STUDY_KQUEUE_H
+#define MUDUO_STUDY_KQUEUE_H
+
+#include "Poller.h"
+#include "log/logger.h"
+
+class kqueue :public Poller{
+    explicit kqueue(EventLoop* loop);
+    void poller_wait(int timeOut, Channelptrlist* activedChannels) override ;
+    void updateChannel(Channelptr) override ;
+    void removeChannel(Channelptr) override ;
+    bool hasChannel(Channelptr) override ;
+
+    ~kqueue() override;
+};
+
+
+#endif //MUDUO_STUDY_KQUEUE_H
+#endif
